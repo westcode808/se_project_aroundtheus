983605e24ddc2ec87daed7d068112a797d5f3459
westley.ragasa <2701ragasa@gmail.com>
2025-09-08 12:09:04 -1000

    Update Profile Button in JS

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,14 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileCloseButton = document.querySelector("#profile-close-button");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
