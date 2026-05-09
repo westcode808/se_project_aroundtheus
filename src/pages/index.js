ef20b9fb96b41b8ebcca25e5ace3420e36bd1942
westley.ragasa <2701ragasa@gmail.com>
2026-05-09 12:47:18 -1000

    Restructure files, and Set up Webpack bundling and building

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Section from "../components/Section.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import UserInfo from "../components/UserInfo.js";
+import "../pages/index.css";
 import { 
   initialCards,
   config,
